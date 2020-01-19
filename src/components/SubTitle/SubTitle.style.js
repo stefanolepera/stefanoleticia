390b23b5a9eba2c40c0ea41f208bb5c46992c949
Stefano Le Pera <stefano.lepera@gmail.com>
2020-01-19 18:09:43 +0000

    added max-width to subTitle

--- src/components/SubTitle/SubTitle.style.js
+++ src/components/SubTitle/SubTitle.style.js
@@ -2,23 +2,24 @@ import styled from 'styled-components';
 import { device } from '../../constants/Devices';
 
 export const SubTitleWrapper = styled.p`
-font-size: 2rem;
-color: #000;
-font-weight: 400;
-margin: 20px 20px;
-
-text-rendering: optimizeLegibility;
-
-@media ${device.mobileS} {
-    font-size: 1rem;
-}
-@media ${device.mobileM} {
-    font-size: 1.2rem;
-}
-@media ${device.mobileL} {
-    font-size: 1.5rem;
-}
-@media ${device.tablet} {
     font-size: 2rem;
-}
+    color: #000;
+    font-weight: 400;
+    margin: 20px 20px;
+    max-width: 1000px;
+
+    text-rendering: optimizeLegibility;
+
+    @media ${device.mobileS} {
+        font-size: 1rem;
+    }
+    @media ${device.mobileM} {
+        font-size: 1.2rem;
+    }
+    @media ${device.mobileL} {
+        font-size: 1.5rem;
+    }
+    @media ${device.tablet} {
+        font-size: 2rem;
+    }
 `;
